43ab5b66d4cd2689f8ccb79e6e69a9ae1e17b6e3
akhil9028 <111130878+akhil9028@users.noreply.github.com>
2023-04-24 16:21:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+ 
+ function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+  
+    if (username == "admin" && password == "password") {
+      alert("Login successful!");
+    } else {
+      alert("Incorrect username or password.");
+    }
+  }
+  
